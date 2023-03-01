d01fbfba5d0df24a739af38c94debe5959de2088
Ing Papi Mich <cjuarez@creze.com>
2023-03-01 15:46:19 -0600

    Funcionamiento Correcto

--- Correo Cobranza.py
+++ Correo Cobranza.py
@@ -1,25 +1,20 @@
-###AQUI
+#Librerias
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
-from datetime import date
 import smtplib
 
-
-today = date.today
 
 # Iniciamos los parámetros del script
 remitente = 'cjuarez@creze.com'
 destinatarios = ['cjuarez@creze.com']
 # destinatarios = ['bprum@creze.com','stabares@creze.com','jahedo@creze.com','sgazca@creze.com','ecervantes@creze.com','caguilar@creze.com','apedroza@creze.com','celine@creze.com','jmartinez@creze.com','cjuarez@creze.com','asantiago@creze.com','pislas@creze.com']
-asunto = f' PAGO PUNTUAL EMPRESA SA DE CV'
-cuerpo = f'Buena tarde. \n\nEstimado Benjamin Enrique Perez Sandi Martinez esperamos se encuentre muy bien, le recordamos que su crédito Creze con contrato C12137CC6358 a nombre de MAQUINARIA Y EQUIPOS DE TERRACERIA SA DE CV , tiene programado su pago puntual el día 28/02/2023 por un monto de $55,985.43; es importante mantener el saldo en la cuenta para que podamos realizar la domiciliación correspondiente; con esto evitar intereses moratorios y mala calificación ante buró de crédito \n\n ¡Agradecemos el compromiso con su crédito! \n\nEn caso de preferir aplicar su pago por transferencia, recuerda que también contamos con esta forma de pago, compartimos los datos bancarios: \n\nNúmero de Cuenta: 0114736958 \n\nBanco: BBVA BANCOMER S.A. IBM \n\nBeneficiario: BANCO ACTINVER SA POR CT DEL FID 4353 \n\nCLABE: 012180001147369582 \n\nRFC: PSC091014U80 \n\nConcepto: C12137CC6358 \n\nPago: $55,985.43 \n\nArea de Cobranza' 
+asunto = f'PAGO PUNTUAL CREZE_DOMICILIACIÓN_MAQUINARIA Y EQUIPOS DE TERRACERIA SA DE CV'
+cuerpo = f'Buena tarde. \n\nEstimado Benjamin Enrique Perez Sandi Martinez esperamos se encuentre muy bien, le recordamos que su crédito Creze con contrato C12137CC6358 a nombre de MAQUINARIA Y EQUIPOS DE TERRACERIA SA DE CV , tiene programado su pago puntual el día 28/02/2023 por un monto de $55,985.43; es importante mantener el saldo en la cuenta para que podamos realizar la domiciliación correspondiente; con esto evitar intereses moratorios y mala calificación ante buró de crédito \n\n ¡Agradecemos el compromiso con su crédito! \n\nEn caso de preferir aplicar su pago por transferencia, recuerda que también contamos con esta forma de pago, compartimos los datos bancarios: \n\nNúmero de Cuenta: 0114736958 \nBanco: BBVA BANCOMER S.A. IBM \nBeneficiario: BANCO ACTINVER SA POR CT DEL FID 4353 \nCLABE: 012180001147369582 \nRFC: PSC091014U80 \nConcepto: C12137CC6358 \nPago: $55,985.43 \nArea de Cobranza' 
 
 ruta_adjunto = 'cb.jpg'
 nombre_adjunto = 'cb.jpg'
-# ruta_adjunto = 'reporte_semanal.pdf'
-# nombre_adjunto = 'reporte_semanal.pdf'
 
 # # Creamos el objeto mensaje
 mensaje = MIMEMultipart()
@@ -53,7 +48,7 @@ sesion_smtp = smtplib.SMTP('smtp.gmail.com', 587)
 sesion_smtp.starttls()
 
 # Iniciamos sesión en el servidor
-sesion_smtp.login() 
+sesion_smtp.login('cjuarez@creze.com','/creze/92/mich') 
 
 # Convertimos el objeto mensaje a texto
 texto = mensaje.as_string()
